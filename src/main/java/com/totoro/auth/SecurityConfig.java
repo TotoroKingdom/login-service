a11892cd96854e28a39613ffd9f268e45c6dfcb1
totoro <1026215903@qq.com>
2022-07-06 09:04:45 +0800

    merge

--- src/main/java/com/totoro/auth/SecurityConfig.java
+++ src/main/java/com/totoro/auth/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 对于登录login 验证码captchaImage 允许匿名访问
                 .antMatchers("/login", "/captchaImage").anonymous()
+                .antMatchers("/hello").anonymous()
                 .antMatchers(
                         HttpMethod.GET,
                         "/*.html",
--- src/main/java/com/totoro/controller/HelloController.java
+++ src/main/java/com/totoro/controller/HelloController.java
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("hello")
-    public String hello(HttpServletRequest request, HttpServletResponse response){
-        HttpSession session = request.getSession();
-        String clientIP = ServletUtil.getClientIP(request);
-        String name = UserAgentUtil.parse(request.getHeader("user-agent")).getBrowser().getName();
-        String result = name + ": " + clientIP;
+    public Map<Object, Object> hello(HttpServletRequest request, HttpServletResponse response){
+        HashMap<Object, Object> result = new HashMap<>();
+        result.put("hello","world");
         return result;
     }
 }
