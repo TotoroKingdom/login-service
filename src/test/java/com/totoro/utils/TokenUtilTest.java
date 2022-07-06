d0d7212290f5fdbdd82afe55ef2ce2b11d3529e3
totoro <1026215903@qq.com>
2022-07-06 09:35:40 +0800

    修复跨域问题

--- src/test/java/com/totoro/utils/TokenUtilTest.java
+++ src/test/java/com/totoro/utils/TokenUtilTest.java
@@ -2,11 +2,20 @@ package com.totoro.utils;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TokenUtilTest {
+
+
+    @Test
+    void getPwd(){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encode = encoder.encode("123456");
+        System.out.println(encode);
+    }
 
     @Test
     void getToken() {
