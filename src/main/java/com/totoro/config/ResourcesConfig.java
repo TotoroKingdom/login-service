06315c5b0e2b1fe322eb31d09d08e7744e0c869a
TotoroKingdom <634581304@qq.com>
2022-07-10 17:10:14 +0800

    关闭后端跨域配置

--- src/main/java/com/totoro/config/ResourcesConfig.java
+++ src/main/java/com/totoro/config/ResourcesConfig.java
@@ -1,6 +1,7 @@
 package com.totoro.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,21 +19,21 @@ public class ResourcesConfig implements WebMvcConfigurer {
     /**
      * 跨域配置
      */
-    @Bean
-    public CorsFilter corsFilter() {
-        log.info("---------------开启跨域配置---------------------------");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        // 设置访问源地址
-//        config.addAllowedOrigin("*");
-        config.addAllowedOriginPattern("*");
-        // 设置访问源请求头
-        config.addAllowedHeader("*");
-        // 设置访问源请求方法
-        config.addAllowedMethod("*");
-        // 对接口配置跨域设置
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        log.info("---------------开启跨域配置---------------------------");
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        // 设置访问源地址
+////        config.addAllowedOrigin("*");
+//        config.addAllowedOriginPattern("*");
+//        // 设置访问源请求头
+//        config.addAllowedHeader("*");
+//        // 设置访问源请求方法
+//        config.addAllowedMethod("*");
+//        // 对接口配置跨域设置
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 }
